20c30e28459b42a3ac61aca5c145fe313406d59d
Coldr0n <martin.garel.528@gmail.com>
2026-05-25 16:56:04 +0200

    changes

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,39 +1,39 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        opts = {},
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        opts = { ensure_installed = { "lua_ls", "rust_analyzer", "pylsp", "jsonls", "texlab" } },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            -- Configure servers (if custom config is needed)
-            vim.lsp.config("lua_ls", {})
-            vim.lsp.config("rust_analyzer", {})
-            vim.lsp.config("pylsp", {})
-            vim.lsp.config("jsonls", {})
-            vim.lsp.config("texlab", {})
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = { ensure_installed = { "lua_ls", "rust_analyzer", "pylsp", "jsonls", "texlab" } },
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- Configure servers (if custom config is needed)
+			vim.lsp.config("lua_ls", {})
+			vim.lsp.config("rust_analyzer", {})
+			vim.lsp.config("pylsp", {})
+			vim.lsp.config("jsonls", {})
+			vim.lsp.config("texlab", {})
 
-            -- Enable LSP servers
-            vim.lsp.enable("lua_ls")
-            vim.lsp.enable("rust_analyzer")
-            vim.lsp.enable("pylsp")
-            vim.lsp.enable("jsonls")
-            vim.lsp.enable("texlab")
+			-- Enable LSP servers
+			vim.lsp.enable("lua_ls")
+			vim.lsp.enable("rust_analyzer")
+			vim.lsp.enable("pylsp")
+			vim.lsp.enable("jsonls")
+			vim.lsp.enable("texlab")
 
-            vim.api.nvim_create_autocmd("LspAttach", {
-                callback = function(args)
-                    local opts = { buffer = args.buf }
-                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-                end,
-            })
-        end,
-    },
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local opts = { buffer = args.buf }
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+				end,
+			})
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,31 +1,30 @@
 return {
-    {
-        "nvimtools/none-ls.nvim",
-        dependencies = {
-            "nvimtools/none-ls-extras.nvim",
-        },
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
 
-        config = function()
-            local null_ls = require("null-ls")
+		config = function()
+			local null_ls = require("null-ls")
 
-            null_ls.setup({
-                sources = {
-                    -- lua
-                    null_ls.builtins.formatting.stylua,
-                    -- rust
-                    require("none-ls.formatting.rustfmt"),
-                    -- python
-                    null_ls.builtins.formatting.black,
-                    null_ls.builtins.formatting.isort,
-                    null_ls.builtins.diagnostics.pylint,
+			null_ls.setup({
+				sources = {
+					-- lua
+					null_ls.builtins.formatting.stylua,
+					-- rust
+					require("none-ls.formatting.rustfmt"),
+					-- python
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.isort,
+					null_ls.builtins.diagnostics.pylint,
 
-                    null_ls.builtins.formatting.prettier,
-                    -- latex
-                    null_ls.builtins.formatting.latexindent,
-                },
-            })
+					-- latex
+					null_ls.builtins.formatting.latexindent,
+				},
+			})
 
-            vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, { desc = "Format buffer" })
-        end,
-    },
+			vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, { desc = "Format buffer" })
+		end,
+	},
 }
